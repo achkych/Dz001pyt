82c3113ee88715b61cef194f4f464e4f16e76e5e
Marat <achkych@gmail.com>
2023-03-18 22:07:13 +0400

    Dz002pyt

--- Dz001pyt.py
+++ Dz001pyt.py
@@ -1,4 +1,4 @@
-#6z
+# 6z
 # n = int(input('Vvedite nomer bileta '))
 # if n > 999999 or n < 100000:
 #     print('Nekorrekynyi nomer bileta')
@@ -22,12 +22,12 @@
 #     else:
 #         print('Bilet obychnyi')
 
-#4z
-#s = int(input('Vvedite kolvo juravlikov '))
+# 4z
+# s = int(input('Vvedite kolvo juravlikov '))
 # (пусть х кол-во жур-ов которых сделал Петя, тогда Сережа тож сделал х жур-ов, а Катя 4 * х. х+х+4 * х =s, отсюда х = s/6)
-#print(f'Петя и Сережа сделали по {int(s/6)} журавлика(ов) каждый, а Катя {int(s/6) * 4}')
+# print(f'Петя и Сережа сделали по {int(s/6)} журавлика(ов) каждый, а Катя {int(s/6) * 4}')
 
-#8z
+# 8z
 # n = int(input('Введите число долек длины шоколада '))
 # m = int(input('Введите число долек ширины шоколада '))
 # k = int(input('Сколько долек вы хотите отломить? '))
@@ -36,14 +36,47 @@
 # else:
 #     print('Разлом только по одной прямой невозможен')
 
-#1z
+# 1z
 # n = input("Vvedite 3 znachnoe chislo: ")
 # n = int(n)
- 
+
 # a = n % 10
 # b = n % 100 // 10
 # c = n // 100
- 
+
 # print("Summa tsifr chisla:", a + b + c)
 
-#Gotovo!
+# Gotovo!
+
+# z14
+# n = int(input('Vvedite nomer: '))
+# i = 0
+# while 2 ** i <= n:
+#      print(2 ** i)
+#      i += 1
+
+# z12
+# s = int(input('Vvedite 1 N chislo x: '))
+# p = int(input('Vvedite 2 N chislo y: '))
+# #s = x + y
+# #p = x * y
+# x1 = int((s - ((-s) ** 2 - 4 * p) ** 0.5) / 2)
+# y1 = int((s + ((-s) ** 2 - 4 * p) ** 0.5) / 2)
+
+# print(x1,y1)
+
+# z10 nesmog
+# n = int(input('Введите количество монет '))
+# orel = reshka = 0
+# for i in range(n):
+#     x = int(input('Орел(1) или решка(0)? '))
+#     if int (x) == 1:
+#         orel += 1
+#     else:
+#         reshka += 1
+# if orel < reshka:
+#     print(f'Переверните {orel} монет с орла на решку, их меньше всего')
+# elif orel == reshka:
+#     print(f'Количество орлов и решек одинаково, по {orel} штук')
+# else:
+#     print((f'Переверните {reshka} монет с решки на орла, их меньше всего'))
