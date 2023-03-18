d3a78caeab014cb1736e0166f7c0b4b9e559f059
Marat <achkych@gmail.com>
2023-03-18 22:11:06 +0400

    Dz002py

---
+++ Dz002pyt.py
@@ -0,0 +1,32 @@
+# z14
+# n = int(input('Vvedite nomer: '))
+# i = 0
+# while 2 ** i <= n:
+#      print(2 ** i)
+#      i += 1
+
+# z12
+# s = int(input('Vvedite 1 N chislo x: '))
+# p = int(input('Vvedite 2 N chislo y: '))
+# #s = x + y
+# #p = x * y
+# x1 = int((s - ((-s) ** 2 - 4 * p) ** 0.5) / 2)
+# y1 = int((s + ((-s) ** 2 - 4 * p) ** 0.5) / 2)
+
+# print(x1,y1)
+
+# z10 nesmog
+# n = int(input('Введите количество монет '))
+# orel = reshka = 0
+# for i in range(n):
+#     x = int(input('Орел(1) или решка(0)? '))
+#     if int (x) == 1:
+#         orel += 1
+#     else:
+#         reshka += 1
+# if orel < reshka:
+#     print(f'Переверните {orel} монет с орла на решку, их меньше всего')
+# elif orel == reshka:
+#     print(f'Количество орлов и решек одинаково, по {orel} штук')
+# else:
+#     print((f'Переверните {reshka} монет с решки на орла, их меньше всего'))
